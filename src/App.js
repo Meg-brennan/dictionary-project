c984bc26be2ac39a278d10d1bb179a4a75d26acf
Meg-brennan <megcbren@gmail.com>
2021-11-02 17:20:58 -0700

    Import bootstrap into src/index.js and tested bootstrap with button in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello from App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Hello from App
+      <a href="#" className="btn btn-primary shadow">
+        button
+      </a>
+    </div>
+  );
 }
-
-export default App;
