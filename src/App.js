da2335e224268cff2b7f1151953b156faaeae0cd
Meg-brennan <megcbren@gmail.com>
2021-11-02 17:46:45 -0700

    Added the search engine; added header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <footer className="text-center">
-          <a
-            href="https://github.com/Meg-brennan/dictionary-project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Coded
-          </a>{" "}
-          by Megan Brennan and{" "}
-          <a
-            href="https://adoring-dubinsky-7cea47.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            hosted on Netlify
-          </a>
+        <header>
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center mt-4">
+          <small>
+            <a
+              href="https://github.com/Meg-brennan/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Coded
+            </a>{" "}
+            by Megan Brennan and{" "}
+            <a
+              href="https://adoring-dubinsky-7cea47.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
