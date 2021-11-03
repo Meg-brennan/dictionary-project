51c009ce6b613678d4519d15855c09f8c490aa14
Meg-brennan <megcbren@gmail.com>
2021-11-02 17:28:17 -0700

    Added footer to app.js, with links to github and netlify

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Hello from App
-      <a href="#" className="btn btn-primary shadow">
-        button
-      </a>
+      <div className="container">
+        <footer className="text-center">
+          <a
+            href="https://github.com/Meg-brennan/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Coded
+          </a>{" "}
+          by Megan Brennan and{" "}
+          <a
+            href="https://adoring-dubinsky-7cea47.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
